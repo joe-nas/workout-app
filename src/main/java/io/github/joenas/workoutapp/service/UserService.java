ac5cbd279f9434061c7eef2a7818aa2dd3538ed6
joe-nas <pintac1984@gmail.com>
2023-11-11 12:47:37 +0100

    update user implementation

--- src/main/java/io/github/joenas/workoutapp/service/UserService.java
+++ src/main/java/io/github/joenas/workoutapp/service/UserService.java
@@ -7,6 +7,10 @@ import io.github.joenas.workoutapp.repository.WorkoutRepository;
 import lombok.Getter;
 import lombok.Setter;
 import org.slf4j.Logger;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,11 +22,12 @@ public class UserService {
 
     private UserRepository userRepository;
     private WorkoutRepository workoutRepository;
+    private MongoTemplate mongoTemplate;
 
-
-    public UserService(UserRepository userRepository, WorkoutRepository workoutRepository) {
+    public UserService(UserRepository userRepository, WorkoutRepository workoutRepository, MongoTemplate mongoTemplate) {
         this.userRepository = userRepository;
         this.workoutRepository = workoutRepository;
+        this.mongoTemplate = mongoTemplate;
     }
 
     Logger logger = org.slf4j.LoggerFactory.getLogger(UserService.class);
@@ -36,5 +41,17 @@ public class UserService {
 
     public List<Workout> findWorkoutsByOauthId(String oauthId) {
         return workoutRepository.findByOauthId(oauthId);
+    }
+
+    public User updateUser(User user, String oauthId) {
+        Query query = new Query();
+        query.addCriteria(Criteria.where("oauthId").is(oauthId));
+
+        Update update = new Update();
+        update.set("username", user.getUsername());
+        update.set("email", user.getEmail());
+        update.set("profilePictureUrl", user.getMetric());
+
+        return mongoTemplate.findAndModify(query, update, User.class);
     }
 }
