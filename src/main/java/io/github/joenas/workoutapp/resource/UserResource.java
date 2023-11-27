9cc9c8499ec1ac0346bc6cab411eb5e25b860f8b
joe-nas <jonas@falcken.de>
2023-11-27 16:01:14 +0100

    added some documentation

--- src/main/java/io/github/joenas/workoutapp/resource/UserResource.java
+++ src/main/java/io/github/joenas/workoutapp/resource/UserResource.java
@@ -34,6 +34,13 @@ public class UserResource {
         return users;
     }
 
+
+    /**
+     * Endpoint to retrieve a user by their OAuth ID.
+     *
+     * @param oauthId the OAuth ID of the user
+     * @return a ResponseEntity containing the user if found, or a 404 status code if not
+     */
     @GetMapping("/user/{oauthId}")
     public ResponseEntity<User> retrieveUserByOauthId(Authentication auth, @PathVariable String oauthId) {
         logger.debug("Retrieving user with {}", auth);
@@ -47,14 +54,33 @@ public class UserResource {
         }
     }
 
+    /**
+     * Endpoint to retrieve all workouts for a user by their OAuth ID.
+     *
+     * @param oauthId the OAuth ID of the user
+     * @return a ResponseEntity containing the list of workouts if found, or a 404 status code if not
+     */
     @GetMapping("/user/{oauthId}/workouts")
     public ResponseEntity<List<Workout>> retrieveWorkoutsByOauthId(@PathVariable String oauthId) {
         logger.debug("🏋️🏋️🏋️ Finding workouts from user with oauthId: {}", oauthId);
         List<Workout> workouts = userService.findWorkoutsByOauthId(oauthId);
+        if (workouts == null) {
+            logger.debug("🏋️🏋️🏋️ No workouts found from user with oauthId: {}", oauthId);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
         return ResponseEntity.status(HttpStatus.OK).body(workouts);
 //        return "Here are workouts from user with oauthId: " + oauthId;
     }
 
+
+
+    /**
+     * Endpoint to create a workouts for a user by their OAuth ID.
+     *
+     * @param oauthId the OAuth ID of the user
+     * @param workout the workout to be created
+     * @return a ResponseEntity containing the created workout if successful, or a 404 status code if the user was not found
+     */
     @PostMapping("/user/{oauthId}/workouts")
     public String createUserWorkout(@PathVariable String oauthId, @RequestBody Workout workout) {
         logger.debug("Creating workout for user with oauthId: {}", oauthId);
@@ -63,6 +89,14 @@ public class UserResource {
         return "Creating workout for user with oauthId: " + oauthId + " and workout: " + newWorkout.toString();
     }
 
+
+    /**
+     * Endpoint to create a new user.
+     *
+     * @param user the user to be created
+     * @return a ResponseEntity containing the created user if successful, or a 409 status code if a user with the same OAuth ID already exists
+     */
+    // Needs validation
     @PostMapping("/user/create")
     public ResponseEntity<User> createUser(@RequestBody User user) {
         logger.debug("🥩🥩🥩 Trying to create user: {}", user.toString());
@@ -79,10 +113,16 @@ public class UserResource {
                         user.getOauthDetails()
                 )
         );
-        logger.debug("🥩🥩🥩 The newUser is: {}", newUser.toString());
+        logger.debug("🥩🥩🥩 The newUser is: {}", newUser);
         return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
     }
 
+    /**
+     * Endpoint to check if a user exists by their OAuth ID.
+     *
+     * @param oauthId the OAuth ID of the user
+     * @return a ResponseEntity with a 200 status code if the user exists, or a 404 status code if not
+     */
     @GetMapping("/user/check/{oauthId}")
     public ResponseEntity<Void> checkIfUserExists(@PathVariable String oauthId) {
         logger.debug("🦝🦝🦝🐮🐮🐮Checking if user with oauthId: {} exists", oauthId);
@@ -97,6 +137,7 @@ public class UserResource {
     }
 
 
+    // testing Roles
     @GetMapping("/security")
     @PreAuthorize("hasRole('USER')")
     public String securityTest(Authentication auth){
@@ -105,6 +146,13 @@ public class UserResource {
         return "Security test";
     }
 
+    /**
+     * Endpoint to update a user.
+     *
+     * @param oauthId the OAuth ID of the user
+     * @param user    the user to be updated
+     * @return a ResponseEntity containing the updated user if successful, or a 404 status code if the user was not found
+     */
     @PutMapping("/user/{oauthId}/profile")
     @PreAuthorize("hasRole('USER')")
     public String updateUser(@PathVariable String oauthId, @RequestBody User user) {
